c2cb8bfb2065ae9236a0d728c0dced228dd6a090
Lauren <lauren.pan@hotmail.com>
2021-08-22 15:06:23 +0800

    hide en version

--- docs/.vuepress/configs/navbar/en_US.ts
+++ docs/.vuepress/configs/navbar/en_US.ts
@@ -1,26 +1,26 @@
 import type { NavbarConfig } from '@vuepress/theme-default'
 
 export const en_US: NavbarConfig = [
-    {
-        text: 'Get Started',
-        link: '/get-started/',
-    },
-    {
-        text: 'Explore',
-        children: [
-            {
-                text: 'Overview',
-                link: '/explore/index.html',
-            },
-            '/explore/personal-cloud/',
-            '/explore/tinkering-networking/',
-            '/explore/hardware-n-intelligence/',
-        ],
-    },
-    {
-        text: 'Knowledge Base',
-        link: '/kb/',
-    },
+    // {
+    //     text: 'Get Started',
+    //     link: '/get-started/',
+    // },
+    // {
+    //     text: 'Explore',
+    //     children: [
+    //         {
+    //             text: 'Overview',
+    //             link: '/explore/index.html',
+    //         },
+    //         '/explore/personal-cloud/',
+    //         '/explore/tinkering-networking/',
+    //         '/explore/hardware-n-intelligence/',
+    //     ],
+    // },
+    // {
+    //     text: 'Knowledge Base',
+    //     link: '/kb/',
+    // },
     {
         text: 'Community',
         link: '/community/',
