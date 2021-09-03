373e9d4a478cc9d12658bb07d48b96248d6ca8e6
John Guan <Guan.Ningchuan@gmail.com>
2021-09-03 11:50:46 +0800

    update explore

--- docs/.vuepress/components/project-card.vue
+++ docs/.vuepress/components/project-card.vue
@@ -2,23 +2,38 @@
   <el-row>
     <el-col>
       <el-card :body-style="{ padding: '0px' }" shadow="hover">
-        <el-row>
+        <el-row class="project-card">
           <el-col style :span="18">
             <div style="padding: 14px">
               <el-row>
                 <span>{{ title }}</span>
               </el-row>
+              <br />
               <el-row>
                 <span>{{ description }}</span>
               </el-row>
+              <br />
               <el-row>
-                <el-button type="primary" size="mini">Let's Go!</el-button>
-                Diffcuty: <el-rate v-model="rate" disabled></el-rate>
+                <el-col :span="6">
+                  <el-button type="primary" size="mini">Let's Go!</el-button>
+                </el-col>
+                <el-col :span="18">
+                  <el-rate
+                    v-model="rate"
+                    :texts="texts"
+                    show-text
+                    disabled
+                  ></el-rate>
+                </el-col>
               </el-row>
             </div>
           </el-col>
-          <el-col :span="6" style="height: 8rem">
-            <img :src="img" />
+          <el-col :span="6">
+            <el-image
+              style="width: 100%; height: 100%"
+              :src="imgurl"
+              fit="cover"
+            ></el-image>
           </el-col>
         </el-row>
       </el-card>
@@ -28,23 +43,43 @@
 
 <script>
 export default {
-    props:{
-        rate: Number,
-        img: String,
-        title: String,
-        description: String,
+  props: {
+    rate: Number,
+    img: String,
+    title: String,
+    description: String,
+  },
+  data() {
+    return {
+      texts: [
+        "SUPER EASY",
+        "RELATIVELY EASY",
+        "EASY",
+        "INTERMEDIATE",
+        "ADVANCED(ish)",
+      ],
+    };
+  },
+  computed: {
+    imgurl() {
+      if (this.img) {
+        return this.img;
+      } else {
+        return "/images/zimaboard/g4.jpeg";
+      }
     },
-    computed:{
-        imglink(){
-            if(this.img){
-                return this.img
-            }else{
-                return "/images/zimaboard/g4.jpeg"
-            }
-        }
-    }
+  },
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.project-card {
+  height: 8rem;
+}
+.el-row {
+  margin-bottom: auto;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
 </style>
--- docs/explore/README.md
+++ docs/explore/README.md
@@ -11,19 +11,6 @@ This Page is Under Construction...
 
 # Explore
 
-<!-- | [Personal Cloud ](./personal-cloud/)                | [Tinkering Networking](./tinkering-networking/)  | [Hardware & Intelligence](./hardware-n-intelligence/)          |
-| ---------------------------------------------------------- | ------------------------------------------------- | -------------------------------------------------------------- |
-| Create Your Unlimited Personal Cloud Storage               | Gaming Across the Regions (Setup VPN Tunnels)     | Managing 50 Smart Gadgets in One Dashboard (HomeBridge / Haas) |
-| Sync Files from Mutiple Devices Automatically              | Gaming without Latency                            | Customize Smart Devices Workflow (Music and Lights)            |
-| VR / 4K Video Center (Downloading / Streaming One for All) | Get Rid Of Annoying Ads                           | Super Dashboard for All Data                                   |
-| Plex is Cool                                               | Remote Access to Your Office / Home               | Connect Esp32, DIY Home Automation(Hardware Tinkering)         |
-| Sorting / Procesing Your Media Assets by One Click         | Boom! Play with PCIe Accessories - Wireless       | Exploring Decentralized Applications, Mining is just the FIRST Step |
-| Build Your Hi-Res Audio Server                             |                                                   | Folding @ Home                                                 |
-| NVR Storage                                                |                                                   | ETH Node                                                       |
-| Boom! Play with PCIe Accessories - RAID                    |                                                   |                                                                |
-| Git Server for Daily Use                                   |                                                   |                                                                |
- -->
-
 <el-tabs type="border-card" style="margin: 1rem 0 0 0;">
   <el-tab-pane>
     <template #label>
