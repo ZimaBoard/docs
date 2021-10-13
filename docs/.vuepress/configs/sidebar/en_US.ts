453a8aca2de61fa59130cf2fef92623a4e05165f
John Guan <Guan.Ningchuan@gmail.com>
2021-10-13 19:22:39 +0800

    update CasaOS Tutorial

--- docs/.vuepress/configs/sidebar/en_US.ts
+++ docs/.vuepress/configs/sidebar/en_US.ts
@@ -1,40 +1,35 @@
 import type { SidebarConfig } from '@vuepress/theme-default'
 
 export const en_US: SidebarConfig = {
-    '/get-started/': [
-        {
-            text: 'Get Started with ZimaBoard',
-            children: [
-                '/get-started/README.md',
-                '/get-started/access-local-storage.md',
-                '/get-started/hands-on-emby.md',
-                '/get-started/hands-on-lychee.md',
-                '/get-started/oculus-meet-zimaboard.md',
-                '/get-started/learn-more.md',
-            ],
-        },
-    ],
-    '/casaos/': [
-        '/casaos/',
-        {
-            text: 'Tutorial',
-            children: [
-                '/casaos/install-app.md',
-            ],
-        },
-    ],
-    '/projects/CreateYourFirstPersonalCloudStorage/': [
-        {
-            text: 'Create Your First Personal Cloud Storage',
-            children: [
-                '/projects/CreateYourFirstPersonalCloudStorage/README.md',
-                // '/projects/CreateYourFirstPersonalCloudStorage/01.md',
-                // '/projects/CreateYourFirstPersonalCloudStorage/02.md',
-                // '/projects/CreateYourFirstPersonalCloudStorage/03.md',
-                // '/projects/CreateYourFirstPersonalCloudStorage/04.md',
-                // '/projects/CreateYourFirstPersonalCloudStorage/complete.md',
-                // '/projects/CreateYourFirstPersonalCloudStorage/next.md',
-            ],
-        },
-    ],
+  '/get-started/': [
+    {
+      text: 'Get Started with ZimaBoard',
+      children: [
+        '/get-started/README.md',
+        '/get-started/access-local-storage.md',
+        '/get-started/hands-on-emby.md',
+        '/get-started/hands-on-lychee.md',
+        '/get-started/oculus-meet-zimaboard.md',
+        '/get-started/learn-more.md',
+      ],
+    },
+  ],
+  '/casaos/': [
+    '/casaos/',
+    '/casaos/tutorial/',
+  ],
+  '/projects/CreateYourFirstPersonalCloudStorage/': [
+    {
+      text: 'Create Your First Personal Cloud Storage',
+      children: [
+        '/projects/CreateYourFirstPersonalCloudStorage/README.md',
+        // '/projects/CreateYourFirstPersonalCloudStorage/01.md',
+        // '/projects/CreateYourFirstPersonalCloudStorage/02.md',
+        // '/projects/CreateYourFirstPersonalCloudStorage/03.md',
+        // '/projects/CreateYourFirstPersonalCloudStorage/04.md',
+        // '/projects/CreateYourFirstPersonalCloudStorage/complete.md',
+        // '/projects/CreateYourFirstPersonalCloudStorage/next.md',
+      ],
+    },
+  ],
 }
---
+++ docs/casaos/tutorial/README.md
@@ -0,0 +1,26 @@
+---
+title: Tutorials
+
+editLink: true
+
+---
+
+# CasaOS Tutorials
+
+![CasaOS](https://raw.githubusercontent.com/IceWhaleTech/logo/main/casaos/casaos_banner_aldeyjarfoss.png)
+
+## Install Your First App
+
+> File Browser
+
+## Manage Your Movies and Shows
+
+> Jellyfin
+
+## Manage Your Gallery
+
+> PhotoPrism
+
+## Manage Your Torrent
+
+> qBittorrent
